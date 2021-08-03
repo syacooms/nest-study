d05402aa3dcdd32572a153bba5236d88f8b1c5d7
syacooms <syacooms@gmail.com>
2021-08-03 19:21:24 +0900

    First commit nestJs 공부한거 올리기

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,39 @@
+import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import { CatsService } from './cats.service';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  // cats/
+  @Get()
+  getAllCat() {
+    return 'all cat';
+  }
+
+  //cats/id
+  @Get(':id')
+  getOneCats() {
+    return 'One cat';
+  }
+
+  @Post()
+  createCat() {
+    return 'create cat';
+  }
+
+  @Put(':id')
+  updateCat() {
+    return 'update cat';
+  }
+
+  @Patch(':id')
+  updatePertialCat() {
+    return;
+  }
+
+  @Delete(':id')
+  deleteCat() {
+    return 'delete service';
+  }
+}
