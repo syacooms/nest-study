ad802ab02242898e059299a2c4651915f102ad50
syacooms <syacooms@gmail.com>
2021-08-04 20:07:21 +0900

    exception & pipe 간단하게

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,35 +1,41 @@
+import { UseFilters, HttpException, Param, ParseIntPipe } from '@nestjs/common';
 import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import { HttpExceptionFilter } from 'src/http-exception.fillter';
 import { CatsService } from './cats.service';
 
 @Controller('cats')
+@UseFilters(HttpExceptionFilter)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
-  // cats/
+  //cats/
   @Get()
   getAllCat() {
-    return 'all cat';
+    throw new HttpException('this expection', 401);
+    //return 'get all cat api';
   }
 
   //cats/id
   @Get(':id')
-  getOneCat() {
-    return 'One cat';
+  getOneCat(@Param('id', ParseIntPipe) param: number) {
+    console.log(param);
+    console.log(typeof param);
+    return 'get one cat api';
   }
 
   @Post()
   createCat() {
-    return 'create cat';
+    return 'get create cat api';
   }
 
   @Put(':id')
   updateCat() {
-    return 'update cat';
+    return 'get update cat api';
   }
 
   @Patch(':id')
-  updatePertialCat() {
-    return;
+  updatePartialCat() {
+    return 'update partial cat api';
   }
 
   @Delete(':id')
